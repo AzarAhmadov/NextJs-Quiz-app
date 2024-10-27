fc7f077de6636ced9b356bb057292cd40db75672
AzarAhmadov <ehmedovh111@gmail.com>
2024-10-27 21:22:03 +0400

    Initial folders

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { ColorModeButton } from "@/components/ui/color-mode";
+import { Container } from "@chakra-ui/react";
+import React from "react";
+
+const page = () => {
+  return (
+    <Container>
+    
+    </Container>
+  );
+};
+
+export default page;
