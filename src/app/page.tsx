34080008fc284d8c71c701436d34fa227ba73e72
AzarAhmadov <ehmedovh111@gmail.com>
2024-10-27 23:14:30 +0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,29 @@
-import { ColorModeButton } from "@/components/ui/color-mode";
-import { Container } from "@chakra-ui/react";
+import Heading from "@/components/common/heading";
+import { Box, Text } from "@chakra-ui/react";
 import React from "react";
 
 const page = () => {
   return (
-    <Container>
-    
-    </Container>
+    <>
+      <Heading />
+      <Box marginTop="99px" width="28.25rem" lineHeight="5rem" color="text-dark-navy">
+        <Box fontSize="4rem" fontWeight="300">
+          Welcome to the{" "}
+          <Text fontWeight="500" opacity="0.8">
+            Frontend Quiz!
+          </Text>
+        </Box>
+        <Text
+          marginTop="48px"
+          fontWeight="400"
+          fontSize="20px"
+          color="text-gray-navy"
+          fontStyle="italic"
+        >
+          Pick a subject to get started.
+        </Text>
+      </Box>
+    </>
   );
 };
 
