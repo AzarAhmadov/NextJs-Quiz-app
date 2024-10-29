67a8928110ea09e47a36fb8fe0a54c7200d30ad3
AzarAhmadov <ehmedovh111@gmail.com>
2024-10-29 16:55:46 +0400

    Update types.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,3 +3,16 @@ import { ReactNode } from "react";
 export type TypeTitle = {
   children: ReactNode;
 };
+
+export type TypeCard = {
+  children: ReactNode;
+  item: {
+    label: string;
+    icon: string;
+    color: string;
+    link: string;
+  };
+  variants:boolean
+  handleClick?: (link: string) => void;
+  icon?: boolean;
+};
