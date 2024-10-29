e8294c795c388ca8b4917946166af7d537a7a403
AzarAhmadov <ehmedovh111@gmail.com>
2024-10-29 16:55:42 +0400

    Create constants.ts

---
+++ src/constants/constants.ts
@@ -0,0 +1,26 @@
+export const categories = [
+  {
+    label: "HTML",
+    icon: "/html.svg",
+    color: "#FFF1E9",
+    link: "html",
+  },
+  {
+    label: "CSS",
+    icon: "/css.svg",
+    color: "#E0FDEF",
+    link: "css",
+  },
+  {
+    label: "Javascript",
+    icon: "/js.svg",
+    color: "#EBF0FF",
+    link: "javascript",
+  },
+  {
+    label: "Accessibility",
+    icon: "/acc.svg",
+    color: "#F6E7FF",
+    link: "accessibility",
+  },
+];
