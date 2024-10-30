e14a3c88e957e88a33bb63335c677b5a88354d4b
AzarAhmadov <ehmedovh111@gmail.com>
2024-10-30 15:31:47 +0400

    Update page.tsx

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,35 +1,135 @@
 "use client";
 
-import Heading from "@/components/common/Heading";
-import { Box, Flex, Text } from "@chakra-ui/react";
+import QuestionArea from "@/components/ui/QuestionArea";
+import { Box, Flex, Text, Button } from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
 import { usePathname } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
+import { getCategory } from "../actions/actions";
+import { useAppDispatch } from "@/lib/utility/hooks";
+import { getCorrectAnswers, getWrongAnswers } from "@/lib/features/quizSlice";
 
-const page = () => {
-  const pathname = usePathname();
+const Page = () => {
+  const slug = usePathname().replace("/", "");
+  const dispatch = useAppDispatch();
+  const [current, setCurrent] = useState(0);
+  const [selectedOption, setSelectedOption] = useState<string | null>(null);
+  const [status, setStatus] = useState(false);
+
+  const {
+    data: category,
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: ["category"],
+    queryFn: () => getCategory(slug),
+    enabled: !!slug,
+  });
+
+  const handleSelect = (option: string) => {
+    setSelectedOption(option);
+    setStatus(false);
+  };
+
+  const handleSubmit = () => {
+    if (!selectedOption) {
+      setStatus(true);
+      return;
+    }
+
+    if (category[slug][current].answer === selectedOption) {
+      dispatch(getCorrectAnswers());
+    } else {
+      dispatch(getWrongAnswers());
+    }
+
+    if (current < category[slug].length - 1) {
+      setCurrent((prev) => prev + 1);
+    }
+    setSelectedOption(null);
+  };
+
+  const variants = ["A", "B", "C", "D"];
+
+  if (isLoading) {
+    return (
+      <Text textAlign="center" marginTop="5rem">
+        Loading...
+      </Text>
+    );
+  }
+
+  if (error) {
+    return <Text color="red.500">Error fetching data: {error.message}</Text>;
+  }
 
   return (
-    <>
-      <Heading pathname={pathname} />
-      <Flex marginTop="85px">
-        <Box width="465px">
+    <Flex justifyContent="space-between" marginTop="85px">
+      <Box>
+        <Box width="29.063rem">
           <Text
             color="text-gray-navy"
             fontStyle="italic"
             fontWeight="400"
-            fontSize="20px"
-            marginBottom="27px"
+            fontSize="1.25rem"
+            marginBottom="1.688rem"
           >
-            Question 6 of 10
+            Question {current + 1} of {category[slug]?.length}
           </Text>
-          <Text color="text-dark-navy" fontWeight="500" fontSize="36px">
-            Which of these color contrast ratios defines the minimum WCAG 2.1
-            Level AA requirement for normal text?
+          <Text color="text-dark-navy" fontWeight="500" fontSize="2.25rem">
+            {category[slug]?.[current]?.question || "Question not available"}
           </Text>
         </Box>
+      </Box>
+
+      <Flex width="35.25rem" direction="column" gap="1.5rem">
+        {category[slug]?.[current]?.options?.length ? (
+          category[slug][current].options.map(
+            (option: string, index: number) => (
+              <QuestionArea
+                selectedOption={selectedOption === option}
+                key={index}
+                handleClick={() => handleSelect(option)}
+                variantsText={variants[index]}
+                variants={true}
+              >
+                {option}
+              </QuestionArea>
+            )
+          )
+        ) : (
+          <Text color="text-gray" fontSize="1.25rem">
+            No options available
+          </Text>
+        )}
+        <Button
+          type="button"
+          onClick={handleSubmit}
+          fontSize="1.5rem"
+          fontWeight="500"
+          bg="#A729F5"
+          paddingY="2rem"
+          borderRadius="1.5rem"
+          _hover={{ opacity: "0.7" }}
+          className="button"
+        >
+          {selectedOption ? "Next question" : "Submit Answer"}
+        </Button>
+
+        {status && (
+          <Box
+            color="#EE5454"
+            fontWeight="400"
+            textAlign="center"
+            fontSize="1.2rem"
+            marginTop="1rem"
+          >
+            Please select an answer
+          </Box>
+        )}
       </Flex>
-    </>
+    </Flex>
   );
 };
 
-export default page;
+export default Page;
